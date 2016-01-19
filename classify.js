f33ca62f0c37a4fd1e7a742d5d5b60b4e48cf4c6
A. Can UGUR <ahmetcanugur@gmail.com>
2016-01-19 20:55:33 +0200

    lst changes
    lst changes

--- classify.js
+++ classify.js
@@ -4,9 +4,9 @@ classifier = new natural.BayesClassifier();
 
 classifier.addDocument('my arm broked', 'search');
 classifier.addDocument('i have broken arm what can i do?', 'search');
-classifier.addDocument('aaaaaaaah', 'search');
-classifier.addDocument('aaaaaaaah', 'search');
-classifier.addDocument('aaaaaaaah', 'search');
+classifier.addDocument('my nose is bleeding', 'search');
+classifier.addDocument('what can i do about broken arm', 'search');
+classifier.addDocument('I have bleeding arm', 'search');
 classifier.addDocument('wake me up qqqq', 'alarm');
 classifier.addDocument('set alarm q\'s', 'alarm');
 classifier.addDocument('qqqq call', 'call');
@@ -17,16 +17,12 @@ classifier.addDocument('where is nearest', 'location');
 classifier.addDocument('where is hospital', 'location');
 classifier.addDocument('where is pharmacy', 'location');
 
-
 classifier.train();
 
 function classify(query,callback) {
   console.log(classifier.getClassifications(query));
-
   return callback(null ,classifier.classify(query))
 }
-
-
 
 module.exports = {
   classify
